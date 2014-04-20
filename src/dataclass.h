8ff31501279145a3a2ccf3aa4823f0feb4c289a3
mrkvincze <mrk.vincze@gmail.com>
2014-04-20 22:43:24 +0200

    missing files

---
+++ src/dataclass.h
@@ -0,0 +1,11 @@
+﻿class MyClass
+{
+public:
+    int IntVar;
+
+    int IntVar2;
+
+    double DoubleVar;
+
+    std::string StringVar;
+}
---
+++ src/generated/textcomponent.h.bind.cc
@@ -0,0 +1,18 @@
+#include <boost/python.hpp>
+#include "textcomponent.h"
+
+using namespace boost::python;
+
+BOOST_PYTHON_MODULE(CodegenExample)
+{
+    class_<TextComponent>("TextComponent")
+        .def("text", &TextComponent::text)
+        .def("setText", &TextComponent::setText)
+    ;
+}
+
+void init_bindings()
+{
+    Py_Initialize();
+    initCodegenExample();
+}
